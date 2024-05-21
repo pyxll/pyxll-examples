d10646781ea95268359e4e42da0ba237f737b806
Tony Roberts <tony@pyxll.com>
2024-05-21 15:06:30 +0100

    Switch matplotlib examples to use PySide6
    And add a function to display and update a matplotlib CTP from an Excel
    worksheet function.

--- matplotlib/interactiveplot.py
+++ matplotlib/interactiveplot.py
@@ -10,15 +10,23 @@ Task Pane feature.
 
 The menu item is configured in the 'ribbon.xml' found in the same
 folder as this file.
+
+This module also contains a worksheet function 'show_matplotlib_ctp'
+which can be called from a worksheet to display a matplotlib Figure
+in a CTP. This second function shows how the widget contained in the
+CTP can be updated as inputs change and the function is re-run.
 """
-from pyxll import create_ctp, xl_app, XLCell, CTPDockPositionFloating
+from pyxll import xl_func, create_ctp, xl_app, xlfCaller, XLCell, CTPDockPositionFloating
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import tkinter as tk
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
+from weakref import WeakValueDictionary
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QApplication
 
 
-def show_matplotlib_ctp(control):
+def show_selected_matplotlib_ctp(control):
     """Context menu action callback.
+
     Gets a matplotlib Figure object from the current Excel selection
     and displays in in a custom task pane.
     """
@@ -32,19 +40,88 @@ def show_matplotlib_ctp(control):
     if not isinstance(fig, Figure):
         raise ValueError("Expected a matplotlib Figure object")
 
-    # Create the top level Tk window and give it a title
-    root = tk.Toplevel()
-    root.title("Matplotlib Plot")
+    # Before we can create a Qt widget the Qt App must have been initialized.
+    # Make sure we keep a reference to this until create_ctp is called.
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication([])
+
+    # Create the widget and a layout for it
+    widget = QWidget()
+    layout = QVBoxLayout(widget)
+    widget.setLayout(layout)
 
     # Add the matplotlib plot to the window
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
+    canvas = FigureCanvas(fig)
+    widget.layout().addWidget(canvas)
 
-    # Add a toolbar to the layout
-    toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
-    toolbar.update()
-    toolbar.pack(side=tk.BOTTOM, fill=tk.X)
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    # And add a toolbar
+    toolbar = NavigationToolbar(canvas)
+    widget.layout().addWidget(toolbar)
 
     # Show as a custom task pane using PyXLL.create_ctp
-    create_ctp(root, width=800, height=800, position=CTPDockPositionFloating)
+    create_ctp(widget, width=800, height=800, position=CTPDockPositionFloating)
+
+
+# Dictionary of calling cell addresses to Qt widgets
+_mpl_ctp_cache = WeakValueDictionary()
+
+
+@xl_func("object fig, bool enabled: var", macro=True)  # macro=True is needed for xlfCaller
+def show_matplotlib_ctp(fig, enabled=True):
+    """Display a matplotlib Figure in a Custom Task Pane.
+
+    This worksheet function takes a cell reference rather than
+    an object directly as it keeps track of the custom task pane
+    and updates it with the new figure if called again for the same
+    cell.
+    """
+    if not enabled:
+        return fig
+
+    if not isinstance(fig, Figure):
+        raise ValueError("Expected a matplotlib Figure object")
+
+    # Get the calling cell to check if there is already a visible CTP for this cell
+    cell = xlfCaller()
+
+    # Get the widget from the cache if it exists already
+    widget = _mpl_ctp_cache.get(cell.address, None)
+    show_ctp = True if widget is None else False
+    if widget is None:
+        # Before we can create a Qt widget the Qt App must have been initialized.
+        # Make sure we keep a reference to this until create_ctp is called.
+        app = QApplication.instance()
+        if app is None:
+            app = QApplication([])
+
+        # Create the widget and a layout for it
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        widget.setLayout(layout)
+
+    # Close any old widgets and remove them from the layout
+    layout = widget.layout()
+    while layout.count() > 0:
+        child = layout.itemAt(0)
+        child.widget().close()
+        layout.removeItem(child)
+
+    # Add the matplotlib plot to the window
+    canvas = FigureCanvas(fig)
+    widget.layout().addWidget(canvas)
+
+    # And add a toolbar
+    toolbar = NavigationToolbar(canvas)
+    widget.layout().addWidget(toolbar)
+
+    # Create and show the CTP if necessary
+    if show_ctp:
+        create_ctp(widget, width=800, height=800, position=CTPDockPositionFloating)
+
+    # We use a WeakValueDict so the item stays in this dict so long as the widget is alive.
+    # Once the CTP is closed and the widget is destroyed then the item in the cache is
+    # cleared automatically.
+    _mpl_ctp_cache[cell.address] = widget
+
+    return fig
